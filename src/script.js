d9ceba524dfd889f2463e1b11ef2ab0644fd2711
Leonel Oliveira <leoneloliver@gmail.com>
2019-03-21 13:18:50 -0400

    Update script.js

--- src/script.js
+++ src/script.js
@@ -12,11 +12,25 @@
       const totalNew = document.getElementsByClassName('new').length;
       document.getElementsByClassName('new-user')[0].innerHTML = totalNew;
       
+     
       var hideShowItem = function(){
         document.getElementsByClassName("all-item")[0].classList.toggle('hide');
       }
       var btnDown = document.getElementsByClassName("btn-down")[0];
       btnDown.addEventListener("click", hideShowItem);
+     
+     
+     var ShowPopUp = function(){
+       alert('Function activated!');
+     }
+     var btnCloud = document.getElementsByClassName("btn-cloud");
+     Array.from(btnCloud).forEach(function(del){
+       del.addEventListener("click", ShowPopUp);
+     });
+     var btnDelete = document.getElementsByClassName("btn-delete");
+     Array.from(btnDelete).forEach(function(clo){
+       clo.addEventListener("click", ShowPopUp);
+     });
 
     },1000);
  }
