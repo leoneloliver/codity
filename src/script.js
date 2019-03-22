f21b117c76afbedb5333e9852821d10e67fac295
Leonel Oliveira <leoneloliver@gmail.com>
2019-03-22 10:57:08 -0400

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,9 +1,9 @@
 
  function myAct(){
     setTimeout(function(){
-      var detail = document.getElementsByClassName("all-info");
-      var divActive = document.getElementsByClassName("active");
-      var showBtn = function() {
+      const detail = document.getElementsByClassName("all-info");
+      const divActive = document.getElementsByClassName("active");
+      const showBtn = function() {
         this.classList.toggle('active');
       }
       Array.from(detail).forEach(function(element){
@@ -13,21 +13,21 @@
       document.getElementsByClassName('new-user')[0].innerHTML = totalNew;
       
      
-      var hideShowItem = function(){
+      const hideShowItem = function(){
         document.getElementsByClassName("all-item")[0].classList.toggle('hide');
       }
-      var btnDown = document.getElementsByClassName("btn-down")[0];
+      const btnDown = document.getElementsByClassName("btn-down")[0];
       btnDown.addEventListener("click", hideShowItem);
      
      
-     var ShowPopUp = function(){
+     const ShowPopUp = function(){
        alert('Function activated!');
      }
-     var btnCloud = document.getElementsByClassName("btn-cloud");
+     const btnCloud = document.getElementsByClassName("btn-cloud");
      Array.from(btnCloud).forEach(function(del){
        del.addEventListener("click", ShowPopUp);
      });
-     var btnDelete = document.getElementsByClassName("btn-delete");
+     const btnDelete = document.getElementsByClassName("btn-delete");
      Array.from(btnDelete).forEach(function(clo){
        clo.addEventListener("click", ShowPopUp);
      });
